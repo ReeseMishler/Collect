8747bf1b185a848730b63b09d780332d1b97052c
{Reese <reese.mishler@ou.edu>
2020-11-04 17:08:34 -0600

    Added a List of integers containing duplicates

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Sets are not ordered. no duplicate Element
+		// HashSet TreeSet implement set
+		// Tree has a structure/order
+		
+		// String as key
+		
+		List<Integer>list1 = new ArrayList<Integer>();
+	      list1.add(100);
+	      list1.add(200);
+	      list1.add(300);
+	      list1.add(400);
+	      list1.add(400);
+	      list1.add(500);
+	      list1.add(600);
+	      list1.add(600);
+	      list1.add(700);
+	      list1.add(400);
+	      list1.add(500);
+	      
 
 	}
 
